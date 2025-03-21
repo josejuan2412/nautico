d1db0661999bca1e5e9c3018f058f8f7094794cd
Jose J. Cruz <jjzcru@gmail.com>
2025-03-20 20:27:49 -0500

    Add typescript support

--- functions/api/[[hono]].js
+++ functions/api/[[hono]].js
@@ -1,24 +1,21 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 import { handle } from "hono/cloudflare-pages";
 
-import { v2ApiRouter } from '#backend/apiv2.js'
+import { v2ApiRouter } from "#backend/apiv2.js";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/api', async (c) => {
-  return c.text('Data api')
-})
+app.get("/api", async (c) => {
+  return c.text("Data api");
+});
 
-// route grouping 
-app.route('/api/v2', v2ApiRouter)
-
+// route grouping
+app.route("/api/v2", v2ApiRouter);
 
 // catch unknown backend routes
-app.get('/api/*', (c) => {
-  return c.text('UNKNOWN ROUTE!')
-})
-
+app.get("/api/*", (c) => {
+  return c.text("UNKNOWN ROUTE!");
+});
 
 // connect hono app with cloudflare pages request handler
 export const onRequest = handle(app);
-
