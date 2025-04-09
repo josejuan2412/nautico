40b593169e269e0c7eb51a9c832422e692022305
Jose J. Cruz <jjzcru@gmail.com>
2025-04-08 22:07:41 -0500

    Rename domain

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 import App from "./App.tsx";
 
 // const API_DOMAIN = "http://localhost:8788";
-const API_DOMAIN = "https://dev.nautico.fennex.dev";
+const API_DOMAIN = "";
 const client = new ApolloClient({
   uri: `${API_DOMAIN}/api/graphql`,
   cache: new InMemoryCache(),
